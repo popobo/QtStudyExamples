4bc02fdbbf6695dcef957eec103368bdfcd763f8
popobo <331361996@qq.com>
2023-07-16 16:49:14 +0800

    1.3

--- C++_GUI_Qt_4/First_Part/main.cpp
+++ C++_GUI_Qt_4/First_Part/main.cpp
@@ -1,14 +1,31 @@
 #include <QApplication>
 #include <QPushButton>
+#include <QSlider>
+#include <QSpinBox>
+#include <QHBoxLayout>
 
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
 
-    QPushButton* button = new QPushButton("Quit");
-    QObject::connect(button, SIGNAL(clicked(bool)), &app, SLOT(quit()));
+    QWidget* window = new QWidget();
+    window->setWindowTitle("Enter Your Age");
 
-    button->show();
+    QSlider* slider = new QSlider(Qt::Horizontal);
+    QSpinBox* spinBox = new QSpinBox();
+
+    slider->setRange(0, 130);
+    spinBox->setRange(0, 130);
+
+    QObject::connect(slider, SIGNAL(valueChanged(int)), spinBox, SLOT(setValue(int)));
+    QObject::connect(spinBox, SIGNAL(valueChanged(int)), slider, SLOT(setValue(int)));
+
+    QHBoxLayout* layout = new QHBoxLayout();
+    layout->addWidget(spinBox);
+    layout->addWidget(slider);
+
+    window->setLayout(layout);
+    window->show();
 
     return app.exec();
 }
